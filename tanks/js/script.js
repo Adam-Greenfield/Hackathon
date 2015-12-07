4f18cb1cac68319ec8a099f1e1f9f7ea300a1e3b
Adam-Greenfield <ajamesgreenfield@gmail.com>
2015-12-07 11:52:05 +0000

    added mouse coordinate variables

--- tanks/js/script.js
+++ tanks/js/script.js
@@ -8,6 +8,13 @@ $(function(){
     HEIGHT = canvas.height;
   }
 
+  function circle(x, y, r){
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, (2* Math.PI));
+    ctx.fill();
+    ctx.closePath();
+  }
+
   function writeMessage(canvas, message) {
     var context = canvas.getContext('2d');
     context.clearRect(0, 0, canvas.width, canvas.height);
@@ -18,16 +25,27 @@ $(function(){
   function getMousePos(canvas, evt) {
     var rect = canvas.getBoundingClientRect();
     return {
-      x: Math.round((evt.clientX-rect.left)/(rect.right-rect.left)*canvas.width),
-      y: Math.round((evt.clientY-rect.top)/(rect.bottom-rect.top)*canvas.height)
+      mouseX: Math.round((evt.clientX-rect.left)/(rect.right-rect.left)*canvas.width),
+      mouseY: Math.round((evt.clientY-rect.top)/(rect.bottom-rect.top)*canvas.height)
     };
   }
-  var canvas = document.getElementById('myCanvas');
-  var context = canvas.getContext('2d');
 
   canvas.addEventListener('mousemove', function(evt) {
     var mousePos = getMousePos(canvas, evt);
-    var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
+    var message = 'Mouse position: ' + mousePos.mouseX + ',' + mousePos.mouseY;
+    mouseX = mousePos.mouseX; 
+    mouseY = mousePos.mouseY;
+    console.log(mouseX, mouseY);
     writeMessage(canvas, message);
+
   }, false);
+
+  
+  canvas.addEventListener('click', function(evt){
+    var mousePos = getMousePos(canvas, evt);
+
+
+  })
+
+  initialize();
 });
