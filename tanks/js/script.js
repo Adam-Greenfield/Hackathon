a99870d0a5ec192ace7e3439648fc2f3c4aeb2d2
kashj22 <kjavaid22@gmail.com>
2015-12-07 20:41:33 +0000

    added levels to canvas with buttons

--- tanks/js/script.js
+++ tanks/js/script.js
@@ -2,11 +2,96 @@ $(function(){
   canvas = document.getElementById('myCanvas');
 
 
+
   function initialize(){
     ctx = canvas.getContext("2d");
     WIDTH = canvas.width;
     HEIGHT = canvas.height;
+
   }
+
+  // var images = ['arctic.jpg', 'desert.jpg', 'grass.jpg', 'moon.jpg', 'space.jpg', 'whitehouse.png' ];
+
+  // $('#myCanvas').css(
+  //   {'background-image': 'url(./css/images/' /*+ images[Math.floor(Math.random() * images.length)]+ ')'*/},
+  //   {'background-size' : "cover"}
+  //   );
+
+//WORKING CODE
+// function arctic(){
+//     $("#myCanvas").css("background-image","url(./css/images/arctic.jpg)");
+//   }
+
+//   function loadimage(){
+//     if (document.getElementById("arctic")){
+//     $('#myCanvas').css(
+//         {'background-image': 'url(./css/images/arctic.jpg' + images[Math.floor(Math.random() * images.length)]+ ')'}
+//         );
+//     } else {
+//       console.log("Not working")
+//     }
+// }
+
+//   var e1 = document.getElementById("arctic");
+//   e1.addEventListener('click', loadimage, false)
+
+//MY CODE
+  //   function loadimage(){
+  //     // if ($('#arctic')){
+  //     // $('#myCanvas').css(
+  //     //     {'background-image': 'url(./css/images/arctic.jpg' /*+ images[Math.floor(Math.random() * images.length)]+ ')'*/}
+  //     //     )
+  //     } else if ($("#desert")) {
+  //       $('#myCanvas').css(
+  //         {'background-image': 'url(./css/images/desert.jpg' /*+ images[Math.floor(Math.random() * images.length)]+ ')'*/}
+  //         );
+  //     } else {
+  //       console.log("Unfinished")
+  //     }
+  // }
+
+    var e1 = document.getElementById("arctic");
+    e1.addEventListener('click', function(){
+      $('#myCanvas').css(
+        {'background-image': 'url(./css/images/arctic.jpg' /*+ images[Math.floor(Math.random() * images.length)]+ ')'*/}
+        )
+      }, false);
+  
+    var e2 = document.getElementById("desert");
+    e2.addEventListener('click', function(){ 
+      $('#myCanvas').css(
+        {'background-image': 'url(./css/images/desert.jpg' /*+ images[Math.floor(Math.random() * images.length)]+ ')'*/}
+        )
+        } , false);
+
+    var e3 = document.getElementById("grass");
+    e3.addEventListener('click', function(){ 
+      $('#myCanvas').css(
+        {'background-image': 'url(./css/images/grass.jpg' /*+ images[Math.floor(Math.random() * images.length)]+ ')'*/}
+        )
+        } , false);
+
+    var e4 = document.getElementById("moon");
+    e4.addEventListener('click', function(){ 
+      $('#myCanvas').css(
+        {'background-image': 'url(./css/images/moon.jpg' /*+ images[Math.floor(Math.random() * images.length)]+ ')'*/}
+        )
+        } , false);
+
+    var e5 = document.getElementById("space");
+    e5.addEventListener('click', function(){ 
+      $('#myCanvas').css(
+        {'background-image': 'url(./css/images/space.jpg' /*+ images[Math.floor(Math.random() * images.length)]+ ')'*/}
+        )
+        } , false);
+
+    var e6 = document.getElementById("whitehouse");
+    e6.addEventListener('click', function(){ 
+      $('#myCanvas').css(
+        {'background-image': 'url(./css/images/whitehouse.png' /*+ images[Math.floor(Math.random() * images.length)]+ ')'*/}
+        )
+        } , false);
+
 
   function circle(x, y, r){
     ctx.beginPath();
